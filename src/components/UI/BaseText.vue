2dc83eeabd4b91ea91f9ac2f0260c7b7d933f361
Alina Alferova <asalferova99@gmail.com>
2024-05-18 04:13:30 +0800

    Added basic text component

---
+++ src/components/UI/BaseText.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  text: Array,
+});
+</script>
+<template>
+  <p v-for="paragraph in text" :key="paragraph" class="text">
+    {{ paragraph }}
+  </p>
+  <p v-if="$slots.default" class="text">
+    <slot></slot>
+  </p>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/mixins.scss";
+@import "@/assets/styles/variables.scss";
+.text {
+  @include default-text-styles;
+  padding-bottom: $text-block-padding;
+}
+</style>
