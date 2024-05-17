ec898a86c73a7a5d5a1733a11753e8c543b5c17d
Alina Alferova <asalferova99@gmail.com>
2024-05-18 03:04:29 +0800

    Added basic date display component

---
+++ src/components/UI/BaseDate.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { formatedDate } from "@/functions";
+import { defineProps } from "vue";
+import { isDateValid } from "/src/validators.js";
+defineProps({
+  date: {
+    type: String,
+    required: true,
+    validator: isDateValid,
+  },
+});
+</script>
+
+<template>
+  <div class="date">
+    <time :datetime="formatedDate(date)" class="date__time"> {{ date }}</time>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/mixins.scss";
+@import "@/assets/styles/variables.scss";
+.date {
+  @include flexbox(row);
+  margin-bottom: 15px;
+  margin-top: auto;
+  &__time {
+    @include default-text-ui-styles;
+    text-transform: capitalize;
+  }
+  @media (max-width: 620px) {
+    &__time {
+      font-size: $font-size-text-medium;
+    }
+  }
+}
+</style>
